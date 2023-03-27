2fbb969633b85df3f99e0ccb4dfceda025fa6e30
ram718 <ramsahith65@gmail.com>
2023-03-27 17:02:37 +0530

    first commit

--- db.js
+++ db.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = mongoose.connect(process.env.mongoURL);
+// https://scary-jade-gosling.cyclic.app
 
 module.exports = { connection };
